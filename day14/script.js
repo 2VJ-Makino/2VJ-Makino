2ab46b7caabfb49e823c5beccd4153113d4baf7d
2VJ-Makino <kohji_makino@mail.toyota.co.jp>
2022-04-26 09:05:40 +0900

    3rd comit

--- day14/script.js
+++ day14/script.js
@@ -20,5 +20,24 @@ function test(actual, expected) {
     }
 }
 
-// //アセスメント2
+
+//ウォームアップ
+//引数に x を取り、関数を返す divide という関数を作成してください。
+//内部関数は引数に y をとり、x を y で割った値を返すようにしてください。
+
+/**
+* @param {number} x
+* @returns {(y: number) => number} 引数として y を取り、x を y で割った値を返す関数
+*/
+function divide(number) {
+    let x = number;
+    return function (y) {
+        return x / y;
+    }
+}
+
+const divide2 = divide(2);
+
+test(divide2(4), 0.5);
+test(divide(2)(1), 2);
 
